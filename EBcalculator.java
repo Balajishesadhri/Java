3b606dbc5b6b264bee8e272ef3db7ba7fda284ab
Balajishesadhri <128590523+Balajishesadhri@users.noreply.github.com>
2023-04-25 12:43:19 +0530

    Add files via upload

---
+++ EBcalculator.java
@@ -0,0 +1,16 @@
+class EBcalculator{
+
+public static void main(String[] Args){
+
+
+EBcalculator reader = new EBcalculator();
+reader.calculator_units();
+
+}
+
+void calculator_units(){
+int units =150;
+int price=2;
+System.out.println(units*price);
+}
+} 
