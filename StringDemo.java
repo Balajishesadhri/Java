d45d4b93b45f218ded4dd816673d777bd514c2fc
Balajishesadhri <128590523+Balajishesadhri@users.noreply.github.com>
2023-05-30 16:12:23 +0530

    Update StringDemo.java

--- StringDemo.java
+++ StringDemo.java
@@ -6,7 +6,33 @@ public class StringDemo {
 		// TODO Auto-generated method stub
 StringDemo st = new StringDemo();
 st.check_start();
-	}
+		st.End_with();
+	}private void End_with() {
+		// TODO Auto-generated method stub
+		
+		String name1 = "sabaribala";
+		String name2 = "bala";
+		boolean ends = true;
+		int len1 = name1.length()-1; 
+		for(int len2 = name2.length()-1; len2>=0;len2--)
+		{
+		  if(name1.charAt(len1)== name2.charAt(len2))
+		  {
+		    len1--;
+		    continue; 
+		  }
+		  else
+		  {
+		    System.out.println("No, not ends with");
+		    ends = false; 
+		    break; 
+		  }
+		}  
+		if(ends == true)
+		{
+		  System.out.println("yes, ends with "+name2);
+		}
+		  }
 	private void check_start() {
 	    // TODO Auto-generated method stub
 	    String name1 = "sabaribala";
